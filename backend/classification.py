8157046df3cd5960c66959e146ca10ed2877c67c
Daniel L <lanieldarson@gmail.com>
2025-04-29 11:10:17 -0700

    More artists added for special songs

--- backend/classification.py
+++ backend/classification.py
@@ -1,57 +1,53 @@
-
 # ========== TRACKED ARTISTS AND SPECIAL SONGS ==========
 
 tracked_artists = {
     
-    #Radiohead
+    # Radiohead
     "radiohead": {
         "special_songs": {
-            "creep": "We Detected Creep. You are a creep... but its ok. ",
-            "exit music (for a film)": "Detected 'Exit Music (for a Film)'. Im sorry you are hurting man....",
+            "creep": "We Detected Creep. You are a creep... but its ok.",
+            "exit music (for a film)": "Detected 'Exit Music (for a Film)'. I'm sorry you are hurting man....",
             "true love waits": "We Detected True Love Waits. Do you need a hug??",
             "true love waits - live in oslo": "We Detected True Love Waits... LIVE IN OSLO?? Like seriously, do you need a hug?"
         }
     },
-    
-    # Weezer (yuck) (joke)
-   "weezer": {
-    "special_songs": {
-        "undone - the sweater song": "Detected 'Undone - The Sweater Song'. You are a nerd. But its ok, we all are.",   
-        "say it ain't so": "Detected 'Say It Ain't So'. You are a loser. But its ok, we all are.",
-    }
-}, 
-    
-    # Elliot Smith
+
+    # Weezer (joke)
+    "weezer": {
+        "special_songs": {
+            "undone - the sweater song": "Detected 'Undone - The Sweater Song'. You are a nerd. But it's ok, we all are.",   
+            "say it ain't so": "Detected 'Say It Ain't So'. You are a loser. But it's ok, we all are."
+        }
+    },
+
+    # Elliott Smith
     "elliott smith": {
         "special_songs": {
             "between the bars": "Detected Elliott Smith's 'Between the Bars'. Hold on tight, buddy.",
-            "miss misery": "Detected 'Miss Misery'. Therapay is a option man..."
+            "miss misery": "Detected 'Miss Misery'. Therapy is an option man..."
         }
     },
-    
-    # The Smiths 
+
+    # The Smiths
     "the smiths": {
         "special_songs": {
             "please, please, please let me get what i want": "You will be fine male manipulator."
         }
     },
-    
+
+    # Clairo
     "clairo": {
         "special_songs": {
-            "juna": "Boy... put down that matcha and grab you a beer. (You are a loser)",
+            "juno": "Boy ... put down that matcha and grab you a beer. (You are a loser)"
         }
     }, 
-    
-    
-"beabadoobee": {    
-    "special_songs": { 
-        "beaches": "Detected 'Beaches'. Why do you really listen to her???"
+
+    # Beabadoobee
+    "beabadoobee": {    
+        "special_songs": { 
+            "beaches": "Detected 'Beaches'. Oh cool how original, whats next, glue song??"
+        }
     }
-}
-
-
-
-
-
-
+    
+    
 }

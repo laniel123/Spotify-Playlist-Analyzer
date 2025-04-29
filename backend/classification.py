276a72c01c10b60577e8a21aa05a4b5b285f8c3a
Daniel L <lanieldarson@gmail.com>
2025-04-29 11:13:42 -0700

    formatting

--- backend/classification.py
+++ backend/classification.py
@@ -6,7 +6,7 @@ tracked_artists = {
     "radiohead": {
         "special_songs": {
             "creep": "We Detected Creep. You are a creep... but its ok.",
-            "exit music (for a film)": "Detected 'Exit Music (for a Film)'. I'm sorry you are hurting man....",
+            "exit music (for a film)": "Detected 'Exit Music (for a Film). I'm sorry you are hurting man....",
             "true love waits": "We Detected True Love Waits. Do you need a hug??",
             "true love waits - live in oslo": "We Detected True Love Waits... LIVE IN OSLO?? Like seriously, do you need a hug?"
         }
@@ -15,16 +15,16 @@ tracked_artists = {
     # Weezer (joke)
     "weezer": {
         "special_songs": {
-            "undone - the sweater song": "Detected 'Undone - The Sweater Song'. You are a nerd. But it's ok, we all are.",   
-            "say it ain't so": "Detected 'Say It Ain't So'. You are a loser. But it's ok, we all are."
+            "undone - the sweater song": "Detected Undone - The Sweater Song. You are a nerd. But it's ok, we all are.",   
+            "say it ain't so": "Detected Say It Ain't So. You are a loser. But it's ok, we all are."
         }
     },
 
     # Elliott Smith
     "elliott smith": {
         "special_songs": {
-            "between the bars": "Detected Elliott Smith's 'Between the Bars'. Hold on tight, buddy.",
-            "miss misery": "Detected 'Miss Misery'. Therapy is an option man..."
+            "between the bars": "Detected Elliott Smith's Between the Bars. Hold on tight, buddy.",
+            "miss misery": "Detected Miss Misery. Therapy is an option man..."
         }
     },
 
@@ -45,7 +45,7 @@ tracked_artists = {
     # Beabadoobee
     "beabadoobee": {    
         "special_songs": { 
-            "beaches": "Detected 'Beaches'. Oh cool how original, whats next, glue song??"
+            "beaches": "Detected Beaches. Oh cool how original, whats next, glue song??"
         }
     }
     
